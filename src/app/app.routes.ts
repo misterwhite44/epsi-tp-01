70857ac46384975b90b294e49f36cb7851c56656
louisboucard <louis44.boucard@gmail.com>
2025-04-24 18:24:18 +0200

    todo4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,10 @@ import { BookListComponent } from './components/book-list/book-list.component';
 import { BookDetailComponent } from './components/book-detail/book-detail.component';
 import { AddBookComponent } from './components/add-book/add-book.component';
 
-// TODO 5 : Créer une route pour la page détail d'un livre
 export const routes: Routes = [
-  { path: '', component: HomeComponent},
+  { path: '', component: HomeComponent },
   { path: 'books', component: BookListComponent },
   { path: 'books/add', component: AddBookComponent },
   { path: 'books/:id', component: BookDetailComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' } 
 ];
