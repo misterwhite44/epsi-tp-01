95f141bde2a81b7f832913405eb291da47760777
louisboucard <louis44.boucard@gmail.com>
2025-04-24 18:30:01 +0200

    todo 5 et 6

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -21,11 +21,14 @@ export class AddBookComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    // Initialisation du formulaire avec des validations
     this.bookForm = this.fb.group({
-      title: ['', Validators.required],
-      author: ['', Validators.required],
-      description: ['', Validators.required],
-      category: ['', Validators.required]
+      title: ['', [Validators.required, Validators.minLength(3)]], // Minimum 3 caractères pour le titre
+      author: ['', [Validators.required, Validators.minLength(3)]], // Minimum 3 caractères pour l'auteur
+      description: ['', [Validators.required, Validators.maxLength(500)]], // Description maximale de 500 caractères
+      category: ['', [Validators.required]],
+      rating: [null, [Validators.min(1), Validators.max(5)]], // Note entre 1 et 5
+      isFavorite: [false]
     });
   }
 
@@ -33,12 +36,16 @@ export class AddBookComponent implements OnInit {
     if (this.bookForm.valid) {
       this.bookService.addBook(this.bookForm.value).subscribe({
         next: () => {
-          this.router.navigate(['/books']);
+          this.router.navigate(['/books']); // Redirige vers la liste des livres après ajout
         },
         error: (err: any) => {
-          console.error('Erreur lors de l\'ajout du livre', err);
+          console.error('Erreur lors de l\'ajout du livre', err); // Affiche une erreur si l'ajout échoue
         }
       });
     }
+  }
+
+  get formControls() {
+    return this.bookForm.controls;
   }
 }
--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, Renderer2, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
   standalone: true
 })
-export class HighlightDirective   {
+export class HighlightDirective implements OnChanges {
   @Input() appHighlight: boolean = false;
-  
-  constructor(private el: ElementRef) {}
 
-  // TODO 24: Modifier la directive pour que le texte soit en gras si l'input est true et appliquer cela au champ title du livre sur les pages book-detail et book-list
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.appHighlight) {
+      this.renderer.setStyle(this.el.nativeElement, 'font-weight', 'bold');
+    } else {
+      this.renderer.removeStyle(this.el.nativeElement, 'font-weight');
+    }
+  }
 }
